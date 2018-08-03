a80500a79cf55caca289fc59b9b601c352e41246
Saksham Shah <40961628+saksham-shah@users.noreply.github.com>
2018-08-03 12:39:45 +0100

    Uncomment some things

--- sketch.js
+++ sketch.js
@@ -9,17 +9,17 @@ function setup() {
         x: 300,
         y: 200
     }
-    // stillPos = {
-    //     x: 300,
-    //     y: 200
-    // }
-    // var gc1 = createGameCam(0, 0, width/2, height);
-    // var gc2 = createGameCam(0, 0, width/2, height);
-    // gc1.follow(pos);
-    // gc2.follow(stillPos);
-    // gcs = [gc1, gc2];
-    gc = createGameCam(0, 0, width, height);
-    gc.follow(pos);
+    stillPos = {
+        x: 300,
+        y: 200
+    }
+    var gc1 = createGameCam(0, 0, width/2, height);
+    var gc2 = createGameCam(0, 0, width/2, height);
+    gc1.follow(pos);
+    gc2.follow(stillPos);
+    gcs = [gc1, gc2];
+    // gc = createGameCam(0, 0, width, height);
+    // gc.follow(pos);
     lastUpdate = Date.now();
 }
 
@@ -35,31 +35,31 @@ function draw() {
     if (keyIsDown(DOWN_ARROW)) pos.y += 5 * dt;
 
 
-    // gcs[0].update();
-    // gcs[1].update();
+    gcs[0].update();
+    gcs[1].update();
 
     background(200);
 
-    // for (var i = 0; i < gcs.length; i++) {
-    //     gc = gcs[i];
-    //     gc.background(0);
-    //     gc.fill(color(255, 0, 0));
-    //     gc.noStroke();
-    //     gc.circle(pos.x, pos.y, 10);
-    //     gc.fill(color(255));
-    //     gc.circle(300, 200, 100);
-    // }
-    // gcs[0].draw(0, 0);
-    // gcs[1].draw(width/2, 0);
+    for (var i = 0; i < gcs.length; i++) {
+        gc = gcs[i];
+        gc.background(0);
+        gc.fill(color(255, 0, 0));
+        gc.noStroke();
+        gc.circle(pos.x, pos.y, 10);
+        gc.fill(color(255));
+        gc.circle(300, 200, 100);
+    }
+    gcs[0].draw(0, 0);
+    gcs[1].draw(width/2, 0);
 
-    gc.update();
-    gc.background(0);
-    gc.fill(color(255, 0, 0));
-    gc.noStroke();
-    gc.circle(pos.x, pos.y, 10);
-    gc.fill(color(255));
-    gc.circle(300, 200, 100);
-    gc.draw(0, 0);
+    // gc.update();
+    // gc.background(0);
+    // gc.fill(color(255, 0, 0));
+    // gc.noStroke();
+    // gc.circle(pos.x, pos.y, 10);
+    // gc.fill(color(255));
+    // gc.circle(300, 200, 100);
+    // gc.draw(0, 0);
 
 
 
@@ -70,6 +70,6 @@ function draw() {
 }
 
 function mousePressed() {
-    gc.gameclip = gc.getGameClip();
+    // gc.gameclip = gc.getGameClip();
     // freeze = !freeze;
 }
